335cdbca23a3549e6ec81b44064f75e58ec1256d
Jonathan Ortega <jortega0389@gmail.com>
2016-09-07 23:36:12 -0500

    adding logo and workin on styles

--- themes/parada/functions.php
+++ themes/parada/functions.php
@@ -18,6 +18,8 @@ $sage_includes = [
   'lib/customizer.php' // Theme customizer
 ];
 
+add_theme_support( 'custom-logo' );
+
 foreach ($sage_includes as $file) {
   if (!$filepath = locate_template($file)) {
     trigger_error(sprintf(__('Error locating %s for inclusion', 'sage'), $file), E_USER_ERROR);
--- themes/parada/templates/header.php
+++ themes/parada/templates/header.php
@@ -1,14 +1,16 @@
 <header class="header">
     <div class="header__wrapper content-wrapper">
         <div class="logo" id="logo">
-            <a href="<?= esc_url(home_url('/')); ?>"><img src="{{ url }}" alt="<?php bloginfo('name'); ?>" /></a>
-        </div>
-        <nav class="navigation">
             <?php
-            if (has_nav_menu('primary_navigation')) :
-              wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
-            endif;
+            if ( function_exists( 'the_custom_logo' ) ) {
+                the_custom_logo();
+            }
             ?>
-        </nav>
+        </div>
+        <?php
+        if (has_nav_menu('primary_navigation')) :
+            wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav', 'container_class' => 'navigation', 'container' => 'nav' ]);
+        endif;
+        ?>
     </div>
 </header>
