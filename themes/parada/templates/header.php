03b90e68ad419349da7c29e3118aacc6b0221378
Jonathan Ortega <jortega0389@gmail.com>
2016-09-07 23:03:15 -0500

    Adding advanced custom fields

--- themes/parada/templates/header.php
+++ themes/parada/templates/header.php
@@ -1,12 +1,14 @@
-<header class="banner">
-  <div class="container">
-    <a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
-    <nav class="nav-primary">
-      <?php
-      if (has_nav_menu('primary_navigation')) :
-        wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
-      endif;
-      ?>
-    </nav>
-  </div>
+<header class="header">
+    <div class="header__wrapper content-wrapper">
+        <div class="logo" id="logo">
+            <a href="<?= esc_url(home_url('/')); ?>"><img src="{{ url }}" alt="<?php bloginfo('name'); ?>" /></a>
+        </div>
+        <nav class="navigation">
+            <?php
+            if (has_nav_menu('primary_navigation')) :
+              wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
+            endif;
+            ?>
+        </nav>
+    </div>
 </header>
