dcf94e2cb0ce5ff9307a13aaabed4df34cbaa535
Jonathan Ortega <jortega0389@gmail.com>
2016-09-09 00:08:44 -0500

    added custom hero image styles for home

--- themes/parada/template-home.php
+++ themes/parada/template-home.php
@@ -5,11 +5,11 @@
 ?>
 
 <?php if( get_field('hero_image') ): ?>
-    <!-- Hero image -->
     <section class="hero">
         <div class="hero__image" style="background-image: url('<?php the_field('hero_image'); ?>')"></div>
         <div class="hero__opacity"></div>
         <div class="hero__tt">
+            <img src="<?php site_icon_url(); ?>" alt="<?php bloginfo('name'); ?> logo" class="hero__logo site__logo">
             <h1 class="hero__title"><?php bloginfo('name'); ?></h1>
         </div>
     </section>
