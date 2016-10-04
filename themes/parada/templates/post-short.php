8b092b594c0375fd27b82392bf61fb90831cf13e
Jonathan Ortega <jortega0389@gmail.com>
2016-10-03 22:41:50 -0500

    Adding featured image on post, also contact form and other plugins

--- themes/parada/templates/post-short.php
+++ themes/parada/templates/post-short.php
@@ -1,5 +1,13 @@
 <article <?php post_class(); ?>>
     <header>
+        <?php if(get_post_thumbnail_id()): ?>
+            <?php
+                $thumb_id = get_post_thumbnail_id();
+                $thumb_url_array = wp_get_attachment_image_src($thumb_id, 'thumbnail-size', true);
+                $thumb_url = $thumb_url_array[0];
+            ?>
+            <img src="<?php echo $thumb_url ?>" alt="" class="featured-image">
+        <?php endif; ?>
         <h1 class="entry-title"><?php the_title(); ?></h1>
         <?php get_template_part('templates/entry-meta'); ?>
     </header>
