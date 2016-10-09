5e5bb1166bc8c63e9a6d756f326697487ef915af
Jonathan Ortega <jortega0389@gmail.com>
2016-10-09 01:11:38 -0500

    Adding more progress

--- themes/parada/page.php
+++ themes/parada/page.php
@@ -1,18 +1,23 @@
-<?php
-/**
- * Template Name: About Template
- */
-?>
 <!-- Hero image -->
 <?php if( get_field('hero_image') ): ?>
     <?php get_template_part('templates/hero-image'); ?>
 <?php endif; ?>
 <!-- /Hero image -->
+<!-- Content -->
+<?php while (have_posts()) : the_post(); ?>
+    <?php get_template_part('templates/content', 'page'); ?>
+<?php endwhile; ?>
+<!-- /Content -->
+<!-- Grid -->
+<?php if( get_field('grid_widget_status') ):
+    get_template_part('templates/product-category-grid');
+endif; ?>
+<!-- /Grid -->
 <!-- Banner block -->
-<?php if( get_field('banner_space') ):
-        $banners = get_field('banner_space');
-        foreach( $banners as $banner ):
-            setup_postdata($banner);
+<?php if( get_field('banner_space') && get_field('banner_status') ):
+    $banners = get_field('banner_space');
+    foreach( $banners as $banner ):
+        setup_postdata($banner);
 ?>
     <div class="banner-cta banner-cta--<?php the_field('banner_layout', $banner->ID) ?>">
         <div style="background-image: url(<?php the_field('banner_image', $banner->ID) ?>)" class="banner-cta__wrapper">
@@ -22,13 +27,8 @@
         </div>
     </div>
 <?php
-            wp_reset_postdata();
-        endforeach;
-    endif;
+        wp_reset_postdata();
+    endforeach;
+endif;
 ?>
 <!-- /Banner block -->
-<!-- Content -->
-<?php while (have_posts()) : the_post(); ?>
-  <?php get_template_part('templates/content', 'page'); ?>
-<?php endwhile; ?>
-<!-- /Content -->
